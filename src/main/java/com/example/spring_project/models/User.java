1cb11a747db70b9bb82fd3c59d1c0611c467b9af
Dalton <dthomason24@gmail.com>
2022-01-31 11:33:57 -0500

    added getters and setters to user model

--- src/main/java/com/example/spring_project/models/User.java
+++ src/main/java/com/example/spring_project/models/User.java
@@ -5,27 +5,59 @@ import javax.persistence.*;
 @Entity
 @Table(name = "users")
 public class User {
-        @Id
-        @Column
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        @Column
-        private String username;
+    @Column
+    private String username;
 
-        @Column
-        private String emailAddress;
+    @Column
+    private String emailAddress;
 
-        @Column
-        private String password;
+    @Column
+    private String password;
 
-        public User() {
-        }
+    public User() {
+    }
 
-        public User(Long id, String username, String emailAddress, String password) {
-            this.id = id;
-            this.username = username;
-            this.emailAddress = emailAddress;
-            this.password = password;
-        }
+    public User(Long id, String username, String emailAddress, String password) {
+        this.id = id;
+        this.username = username;
+        this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
